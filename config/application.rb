7852f67a130343afb11513978183b5d36bb594bd
Emmanuel D'ANGELO <emmanuel.dangelo@gmail.com>
2015-04-28 12:39:08 +0200

    Copied applications.rb and routes.rb from kendocup/dummy

--- config/application.rb
+++ config/application.rb
@@ -1,15 +1,27 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+# Pick the frameworks you want:
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "sprockets/railtie"
+require "prawn"
+require "prawn/table"
+require "omniauth"
+require "omniauth-facebook"
+# require "rails/test_unit/railtie"
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+require "kendocup"
 
 module DemoKendocup
   class Application < Rails::Application
+
+    config.autoload_paths += %W( #{config.root}/lib )
+
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
+    	# Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
@@ -18,6 +30,8 @@ module DemoKendocup
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :en
+
+    config.action_mailer.default_url_options = { only_path: true }
   end
 end
